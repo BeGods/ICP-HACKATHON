f5e4a5cdded2769f934978e2d77a6e45981cbec4
tanmay2626 <tanmaywaykar17@gmail.com>
2025-05-16 16:27:36 +0530

    feat: update header for explore & add mythopedia

--- src/components/ror/BookCrd.jsx
+++ src/components/ror/BookCrd.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import HTMLFlipBook from "react-pageflip";
-
-const backgroundImageUrl =
-  "https://media.publit.io/file/BattleofGods/FoF/Assets/CARDS/320px-info-background.jpg";
+import { gameItems } from "../../utils/gameItems";
 
 const BookCrd = ({ buttonColor, myth, assets, handleClose }) => {
+  let relicItems = gameItems.filter(
+    (itm) => itm.id?.includes("relic") && itm.id?.includes(myth)
+  );
+  relicItems = [
+    { id: "", name: "", coins: " ", fragments: [], orbs: "celtc" },
+    ...relicItems,
+  ];
+
   return (
     <div className="fixed inset-0 z-50 flex flex-col gap-y-2 justify-center items-center bg-black bg-opacity-85 backdrop-blur-[3px]">
       <div className="w-4/5 aspect-[4/5]">
@@ -20,44 +26,49 @@ const BookCrd = ({ buttonColor, myth, assets, handleClose }) => {
           autoSize={true}
           className="w-full h-full"
         >
-          {[0, 1, 2, 3, 4].map((page) => (
-            <div className="w-full h-full">
-              {page == 0 ? (
-                <div
-                  key={0}
-                  className="relative w-full h-full text-white rounded-2xl"
-                >
-                  <img
-                    src={backgroundImageUrl}
-                    alt={`page ${page}`}
-                    className="rounded-2xl h-full w-full object-cover"
-                  />
-                  <div className="absolute inset-0 flex flex-col items-center justify-center p-4 text-2xl font-bold text-card">
+          {relicItems.map((page, idx) => {
+            const code = page.id?.replace("relic", "char");
+            console.log(code);
+
+            return (
+              <div className="w-full h-full">
+                {idx == 0 ? (
+                  <div
+                    key={0}
+                    className="relative w-full h-full text-white rounded-2xl"
+                  >
                     <img
-                      src={assets.symbols[myth]}
-                      alt="symbol"
-                      className="opacity-60 symbol-svg w-3/4"
+                      src={`https://media.publit.io/file/BattleofGods/FoF/Assets/CARDS/320px-info-background.jpg`}
+                      alt={`page ${idx}`}
+                      className="rounded-2xl h-full w-full object-cover"
                     />
-                    <h1 className="uppercase text-num mt-8">{myth}</h1>
+                    <div className="absolute inset-0 flex flex-col items-center justify-center p-4 text-2xl font-bold text-card">
+                      <img
+                        src={assets.symbols[myth]}
+                        alt="symbol"
+                        className="opacity-60 symbol-svg w-3/4"
+                      />
+                      <h1 className="uppercase text-num mt-8">{myth}</h1>
+                    </div>
                   </div>
-                </div>
-              ) : (
-                <div
-                  key={page}
-                  className="relative w-full h-full text-white rounded-2xl"
-                >
-                  <img
-                    src={backgroundImageUrl}
-                    alt={`page ${page}`}
-                    className="rounded-2xl h-full w-full object-cover"
-                  />
-                  <div className="absolute inset-0 flex items-start justify-center p-4 text-2xl font-bold text-card">
-                    Page {page}
+                ) : (
+                  <div
+                    key={idx}
+                    className="relative w-full h-full text-white rounded-2xl"
+                  >
+                    <img
+                      src={`/assets/mythopedia/320px-${code}_info_page.jpg`}
+                      alt={`page ${idx}`}
+                      className="rounded-2xl h-full w-full object-cover"
+                    />
+                    <div className="absolute inset-0 flex items-end justify-center p-2 text-2xl font-bold text-card">
+                      Page {idx}
+                    </div>
                   </div>
-                </div>
-              )}
-            </div>
-          ))}
+                )}
+              </div>
+            );
+          })}
         </HTMLFlipBook>
       </div>
       <div
@@ -66,7 +77,7 @@ const BookCrd = ({ buttonColor, myth, assets, handleClose }) => {
       >
         <img src={assets.buttons[buttonColor]?.on} alt="button" />
         <div className="absolute z-50 uppercase text-white opacity-80 text-black-contour font-fof font-semibold text-[1.75rem] mt-[2px]">
-          Exit
+          Close
         </div>
       </div>
     </div>
