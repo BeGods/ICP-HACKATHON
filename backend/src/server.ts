29923cda2cc75feeb06f50595472a0c77feef0cb
tanmay2626 <tanmaywaykar17@gmail.com>
2025-06-23 02:19:42 +0530

    feat: update add socket and update withdraw

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
 import config from "./config/config";
 import app from "./app";
 import connectMongo from "./config/database/mongo";
+import http from "http";
+import { initSocket } from "./config/socket";
 
 const PORT = config.server.PORT;
 
 connectMongo();
 
-app.listen(PORT, (): void => {
+const server = http.createServer(app);
+
+initSocket(server);
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
