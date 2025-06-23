4a489fd588ecf85b348000464c51a072a2b63cd8
tanmay2626 <tanmaywaykar17@gmail.com>
2025-06-23 14:50:13 +0530

    feat: disable sockets

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,11 +1,11 @@
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_SERVER,
-    {
-        transports: ["websocket"],
-        withCredentials: true,
-    }
-);
+// export const socket = io(import.meta.env.VITE_SERVER,
+//     {
+//         transports: ["websocket"],
+//         withCredentials: true,
+//     }
+// );
 
 
 
