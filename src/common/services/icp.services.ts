13251f6bae270aebd7af8c9efe1f09eca3d180ec
tanmay2626 <tanmaywaykar17@gmail.com>
2025-08-26 17:51:41 +0530

    feat: add quest packet mint controlelrs

--- src/common/services/icp.services.ts
+++ src/common/services/icp.services.ts
@@ -1,7 +1,9 @@
+import { Principal } from "@dfinity/principal";
 import { initializeICPActor } from "../../config/icp";
 
-export const registerFOFQuestCmpl = async (principalId) => {
+export const registerFOFQuestCmpl = async (user, mythologyName) => {
   try {
+    const principalId = user.principalId;
     const { backendActor, userPrincipal } = await initializeICPActor(
       principalId
     );
@@ -9,6 +11,43 @@ export const registerFOFQuestCmpl = async (principalId) => {
     if (!backendActor || !userPrincipal) {
       throw new Error("Failed to initalize backend actor");
     }
+
+    const principal = Principal.fromText(principalId);
+
+    console.log(principal, mythologyName);
+
+    const testResult2 = await backendActor.registerQuestCompletion(
+      principal,
+      mythologyName
+    );
+
+    console.log(testResult2);
+    return testResult2;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const mintQuestPackets = async (user, mythology) => {
+  try {
+    const principalId = user.principalId;
+    const { backendActor, userPrincipal } = await initializeICPActor(
+      principalId
+    );
+
+    if (!backendActor || !userPrincipal) {
+      throw new Error("Failed to initalize backend actor");
+    }
+
+    const principal = Principal.fromText(principalId);
+
+    const testResult2 = await backendActor.mintPacketToUser(
+      principal,
+      mythology
+    );
+
+    console.log(testResult2);
+    return testResult2;
   } catch (error) {
     console.log(error);
   }
